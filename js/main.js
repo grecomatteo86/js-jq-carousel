8f0871e7559b094a07f4baf56a96b8dbaa2b8fdf
Matteo <grecomatteo86@gmail.com>
2021-03-12 16:53:36 +0100

    if control on next click (first/last img) commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 $(function(){
 
   var next = $('.next');
-  
+
 
   next.click(
-
     function(){
 
       var imgActive = $('.images img.active');
-      imgActive.removeClass('active');
-      imgActive.next().addClass('active');
+      var imgLast = $('.images img.last');
+      var imgFirst = $('.images img.first');
+
+      if (imgLast.hasClass('active')) {
+
+        imgActive.removeClass('active');
+        imgFirst.addClass('active');
+
+      } else {
+
+        imgActive.removeClass('active');
+        imgActive.next().addClass('active');
+
+      }
 
     }
-
+    
   );
 
 });
